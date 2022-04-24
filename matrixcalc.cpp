508ceaf4320384f64d90d0ec9d898df9e6314e43
Matthew Mullen <matthew.mullenx@gmail.com>
2022-04-24 15:52:13 -0600

    bear content adjustment

--- matrixcalc.cpp
+++ matrixcalc.cpp
@@ -1,27 +1,11 @@
 #include <iostream>
 #include <vector>
+#include <string>
 
-using namespace std;
+
 int main(){
-    long m, n;
-    cin>>m;
-    cin>>n;
-    vector<vector<long>> matrix;
-    for(int i=0;i<m;++i){
-        for(int j=0;j<n;++j){
-            long k;
-            cin>>k;
-            matrix[i].push_back(k);
-        }
-    }
-    long m2,n2;
-    vector<vector<long>> matrix2;
-    for(int i=0;i<m2;++i){
-        for(int j=0;j<n2;++j){
-            long k;
-            cin>>k;
-            matrix2[i].push_back(k);
-        }
-    }
+    std::string str = "Hello World!";
+    str[str.size()-1]='?';
+    std::cout<<str<<std::endl;
     
 }
